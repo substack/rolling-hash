440bf3eaa87e232db6800791b3fa03778abac827
James Halliday <mail@substack.net>
2014-03-24 00:33:48 -0700

    command-line program

---
+++ bin/cmd.js
@@ -0,0 +1,26 @@
+var rolling = require('../');
+var fs = require('fs');
+var minimist = require('minimist');
+
+var argv = minimist(process.argv.slice(2), {
+    alias: {
+        e: 'encoding',
+        a: 'algorithm',
+        s: 'size'
+    },
+    default: {
+        encoding: 'hex',
+        algorithm: 'md5',
+        size: 1024 * 16
+    }
+});
+var input = argv._[0] && argv._[0] !== '-'
+    ? fs.createReadStream(argv._[0])
+    : process.stdin
+;
+
+var rh = rolling(argv);
+rh.on('hash', function (h) {
+    console.log(h);
+});
+input.pipe(rh);
--- example/roll.js
+++ example/roll.js
@@ -2,7 +2,7 @@ var rolling = require('../');
 var fs = require('fs');
 var file = process.argv[2];
 
-var rh = rolling('md5', 1024 * 128);
+var rh = rolling('md5', 1024 * 8);
 rh.on('hash', function (h) {
     console.log(h.toString('hex'));
 });
